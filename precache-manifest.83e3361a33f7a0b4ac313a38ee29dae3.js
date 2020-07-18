d7aa899a21807debcc2ceb9c06209328c02fb06f
PingKee <pingkee.ng@gmail.com>
2020-07-18 21:13:13 +0800

    Updates

---
+++ precache-manifest.83e3361a33f7a0b4ac313a38ee29dae3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc7e66e8e666511ade676c68bb6d787b",
+    "url": "/MWC-webclient/index.html"
+  },
+  {
+    "revision": "0428de75816b3c508509",
+    "url": "/MWC-webclient/static/css/main.80ab14d3.chunk.css"
+  },
+  {
+    "revision": "6978bf57174362fc522a",
+    "url": "/MWC-webclient/static/js/2.0bacf801.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MWC-webclient/static/js/2.0bacf801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0428de75816b3c508509",
+    "url": "/MWC-webclient/static/js/main.a870e32c.chunk.js"
+  },
+  {
+    "revision": "de7824d9f12f459e4666",
+    "url": "/MWC-webclient/static/js/runtime-main.5ea8d453.js"
+  }
+]);
